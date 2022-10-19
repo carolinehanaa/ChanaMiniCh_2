d14647ee24a370d4c23c88f1c84e513e6e9e20a6
carolinehanaa <carolinehana28@gmail.com>
2022-10-19 10:31:24 -0700

    First Push

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//Caroline Hana
+//10-19-2022
+//Add two numbers
+//Peer Review- 
+
+Console.Clear();
+string? playAgain = "yes";
+while (playAgain == "yes")
+{
+
+    string? input1, input2;
+    int number1, number2, sum;
+
+    Console.Write("Put a number: ");
+    input1 = Console.ReadLine();
+
+    Console.Write("Put another number: ");
+    input2 = Console.ReadLine();
+
+    bool validnum1 = int.TryParse(input1, out number1);
+    bool validnum2 = int.TryParse(input2, out number2);
+
+    if (!validnum1 || !validnum2)
+    {
+        Console.WriteLine("Invalid Number");
+        playAgain = "yes";
+    }
+
+    else if (validnum1 && validnum2)
+    {
+        sum = number1 + number2;
+        Console.WriteLine($"{number1} + {number2} = {sum}");
+    }
+
+    string? answer = "";
+    while (answer != "yes" && answer != "no")
+    {
+        Console.WriteLine("Would you like to play again? (yes or no)");
+        answer = Console.ReadLine();
+        answer = answer.ToLower();
+        if (answer == "yes")
+        {
+            playAgain = answer;
+        }
+        else if (answer == "no")
+        {
+            playAgain = answer;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Answer");
+        }
+
+    }
+
+
+}
